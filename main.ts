90b220dd2dd32750c348010f27daa31d1ac1a74d
Warrenharper-28 <80335111+BRUH-ling@users.noreply.github.com>
2022-10-18 09:54:18 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,14 +1,22 @@
 input.onButtonPressed(Button.A, function () {
     radio.sendNumber(1)
+    basic.pause(1000)
+    radio.sendNumber(0)
 })
 input.onGesture(Gesture.TiltLeft, function () {
     radio.sendNumber(3)
+    basic.pause(1000)
+    radio.sendNumber(0)
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(2)
+    basic.pause(1000)
+    radio.sendNumber(0)
 })
 input.onGesture(Gesture.TiltRight, function () {
     radio.sendNumber(4)
+    basic.pause(1000)
+    radio.sendNumber(0)
 })
 radio.setGroup(1)
 basic.forever(function () {
